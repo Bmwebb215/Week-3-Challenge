c8b856aef036f3bed47cbfb2a0d03734d78e4750
Bdubs <bmwebb9@gmail.com>
2023-10-22 12:01:28 -0400

    completed javascript code and ReadMe file

--- Develop/script.js
+++ Develop/script.js
@@ -1,36 +1,39 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-/**
- Prompt the use
- */
-var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-// var numbers = "0123456789"
-// console.log(numbers.split(""));
+
+var upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz';
+var numbers = '0123456789';
+var specialCharacters = '!@#$%^&*()-_+=<>?';
 
 function generatePassword() {
-
   var userSet = ""
   var length = prompt("Choose the number of characaters between 8 and 128.");
-  if(length < 8){
+  if (length < 8) {
     alert("Password must be at least 8 characters");
     return
   } else if (length > 128) {
     alert("Password must be less than 128 characters");
     return
   }
-  var hasUpperCase = confirm(" Would you like uppercase in your password?")
-  var hasLowerCase = confirm("Would you like to have lower case in your password");
+  var hasUpperCase = confirm(" Would you like  to include uppercase letters?");
+  var hasLowerCase = confirm("Would you like to include lower case letters?");
+  var hasNumbers = confirm("Would you like to include numbers?")
+  var hasSpecialChar = confirm("Would you like to include special characters?");
 
-  if (hasUpperCase === true) {
-    userSet += upperCaseLetters
+  if(hasUpperCase) {userSet += upperCaseLetters};
+  if(hasLowerCase) {userSet += lowerCaseLetters};
+  if(hasSpecialChar) {userSet += specialCharacters};
+  if(hasNumbers){userSet += numbers};
+
+
+var madePassword = "";
+  for (let i = 0; i < length; i++) {
+var index = Math.floor(Math.random() * userSet.length);
+madePassword += userSet[index];
 
   }
-  // for (let i = 0; i < length; i++) {
-
-  //  var index = Math.floor(Math.random() * upperCaseLetters.length);
-
-  // }
+  return madePassword
 }
 
 // Write password to the #password input
@@ -44,4 +47,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-//var s = ' !"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Week-3-Challenge
+
+## User Story
+```
+AS AN employee with access to sensitive data
+I WANT to randomly generate a password that meets certain criteria
+SO THAT I can create a strong password that provides greater security
+```
+## Usage 
+
+When the user loads the page they see a blank box and a button which states 'Generate Password'
+When the button is clicked the user is presented with a series of prompts. First, the user is asked to choose a length for the password. The user must select a number between 8 and 128 if any number is selected outside of these parameters the user with be alerted and must restarted the application by clicking 'Generate Password'
+When the selection of the length is valid the user must then confirm if they would like to include uppercase letters, lower case letters, numbers and special characters. The application will then produce a result based on what the user has selected.
